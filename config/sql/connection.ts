5855b3a321fe31949b6338f17b8f77e378221c83
wajeehamushtaq <wajeehamushtaq1@gmail.com>
2024-03-23 15:17:23 +0500

    chore: add env example file

--- config/sql/connection.ts
+++ config/sql/connection.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const datasource = new DataSource({
   type: "mysql",
-  host: process.env.SQL_HOST || "localhost",
+  host: process.env.SQL_HOST || "127.0.1",
   port: parseInt(process.env.SQL_PORT || "3306"),
   username: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
